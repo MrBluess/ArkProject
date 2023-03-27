41dbef643c82ed3fcf6dc742d17493194e2c2def
Ramiro Garcia <yosoysaban@gmail.com>
2023-03-26 20:18:08 -0600

    Prueba para enviar correo electronico

--- Mensaje.py
+++ Mensaje.py
@@ -3,9 +3,9 @@ import smtplib
 # Configurar servidor SMTP
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
-smtp_username = 'sopadecaracol123123@gmail.com'
-smtp_password = 'Saban1808*'
-smtp_sender = 'yosoysaban@gmail.com'
+smtp_username = '********'
+smtp_password = '********'
+smtp_sender = '**********'
 
 # Crear mensaje
 message = """\
